a7414fab6c86a6c596a0e8f506a7cfae1be69a3f
brianzenhom <brianzenhom@hotmail.com>
2023-05-15 00:12:37 +0200

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,24 @@ module.exports = {
     fontFamily: {
       sans: ["Gotham SSm A, sans-serif"],
     },
-    extend: {},
+    extend: {
+      animation: {
+        tilt: "tilt 10s infinite linear",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(1deg)",
+          },
+          "75%": {
+            transform: "rotate(-1deg)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
